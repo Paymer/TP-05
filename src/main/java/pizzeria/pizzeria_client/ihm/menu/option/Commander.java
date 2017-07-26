2775d2a1b240d46db99f526a6dc452d24ac67973
Paula Aymerich <p.aymerich.f@gmail.com>
2017-07-26 17:36:54 +0200

    26-07-2017-1

--- src/main/java/pizzeria/pizzeria_client/ihm/menu/option/Commander.java
+++ src/main/java/pizzeria/pizzeria_client/ihm/menu/option/Commander.java
@@ -1,9 +1,16 @@
 package pizzeria.pizzeria_client.ihm.menu.option;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
+import pizzeria.central.Pizza;
+import pizzeria.pizzeria_admin.dao.IPizzaDao;
+import pizzeria.pizzeria_admin.dao.PizzaDaoAPI;
 import pizzeria.pizzeria_client.dao.Statut;
 import pizzeria.pizzeria_client.dao.client.Client;
 import pizzeria.pizzeria_client.dao.client.IClientDao;
@@ -25,16 +32,43 @@ public class Commander implements OptionMenuClient {
 		Commande commande = new Commande ();
 		
 		
+
+		
 		commande.setClient((Client) clDao);
-		co.console("Introduir prenom");
-		commande.setDateCommande();
+		commande.setDateCommande(LocalDateTime.now());
 		//commande.setLivreur(livreur); there is no livreur right now
 		commande.setNumeroCommande(01);
 		commande.setStatut(Statut.INIT);
-		
-		
-		co.console("Introduir password");
+
+		List<Pizza> comm = new ArrayList<>();
 	
+		IPizzaDao list = new PizzaDaoAPI();
+		list.init();
+		
+		List<Pizza> list2 = new ArrayList<>();
+		list2 = list.getPizzas();
+		
+		co.console("Pizzas List");
+		co.console(list2.toString());
+		co.console("introduce the id of the pizza of you want");
+		co.console("to stop write 'a'");
+		
+		//CAHOS SEARCH HOW TO DO THAT!!!!
+		int c = -1;
+		do{
+			String ch = scanner.nextLine();
+			
+			try {c = Integer.parseInt(ch);
+			//it should take all the pizzas 
+			if ( c >= 0 && c < list2.size()){
+			comm.add(list2.get(c));
+			}
+			
+			}catch{
+				//TODO in case it is not possible to transform the string into a integer
+			}
+			
+		}while (c != 'a');
 		
 		
 	}
